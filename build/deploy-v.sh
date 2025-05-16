e3d3278ba3c179bb572028ab5b8002cf03891a76
Wire <kerigancreighton@gmail.com>
2025-05-15 23:10:48 -0500

    Update deploy-v.sh to use container 3

--- build/deploy-v.sh
+++ build/deploy-v.sh
@@ -19,20 +19,12 @@ if [[ "$(uname -a)" == *"Darwin"* ]]; then
     ./project/victor/scripts/victor_start.sh
 else
 
-docker build \
---build-arg DIR_PATH="$(pwd)" \
---build-arg USER_NAME=$USER \
---build-arg UID=$(id -u $USER) \
---build-arg GID=$(id -g $USER) \
--t vic-standalone-builder-2 \
-build/
-
 docker run -it \
     -v $(pwd)/anki-deps:/home/$USER/.anki \
     -v $(pwd):$(pwd) \
     -v $(pwd)/build/cache:/home/$USER/.ccache \
     -v /home/$USER/.ssh:/home/$USER/.ssh \
-    vic-standalone-builder-2 bash -c \
+    vic-standalone-builder-3 bash -c \
     "cd $(pwd) && \
     eval \$(ssh-agent) && \
     ssh-add robot_sshkey && \
