a7b7fdc85a99840f50ece644f54c075ff793193b
Kerigan <kerigancreighton@gmail.com>
2025-05-19 03:14:38 -0500

    Model creation instructions

---
+++ 3rd/picovoice/x86_64-linux/create-model.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [[ ! -d oldpork ]]; then
+	wget https://archive.org/download/github.com-Picovoice-Porcupine_-_2018-10-07_02-31-11/Picovoice-Porcupine_-_2018-10-07_02-31-11.bundle
+	git clone Picovoice-Porcupine_-_2018-10-07_02-31-11.bundle oldpork
+	rm Picovoice-Porcupine_-_2018-10-07_02-31-11.bundle
+fi
+
+./pv_porcupine_optimizer -r $(pwd)/oldpork/resources -o $(pwd) -p linux -w "$1"
