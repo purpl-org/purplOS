d870bf2145af9bdbf05e60b1b0eb6e0085a81aa9
froggitti <85637569+froggitti@users.noreply.github.com>
2025-06-08 15:16:48 -0400

    instead of relying on the user to input the robot ip before deploying, allow them to do so via the deploy script

--- build/deploy-v.sh
+++ build/deploy-v.sh
@@ -4,10 +4,22 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
 echo $DIR
 cd $DIR
 
-if [[ ! -f robot_ip.txt ]]; then
-    echo "You must echo your robot's IP to robot_ip.txt in the root of this repo before running this script."
-    exit 1
+if [ -f "robot_ip.txt" ]; then
+    rm robot_ip.txt
 fi
+
+read -p "Enter robot IP: " robotip
+
+export ROBOT_IP="$robotip"
+
+echo $ROBOT_IP >> robot_ip.txt
+
+echo "Deploying to $ROBOT_IP"
+
+#if [[ ! -f robot_ip.txt ]]; then
+#    echo "You must echo your robot's IP to robot_ip.txt in the root of this repo before running this script."
+#    exit 1
+#fi
 
 if [[ ! -f robot_sshkey ]]; then
     echo "You must copy your robot's SSH key the root of this repo with the name robot_sshkey before running this script."
