fa0643b0919d9ec45edcbbb9f65844b98433feea
froggitti <85637569+froggitti@users.noreply.github.com>
2025-07-21 01:13:30 -0400

    lower wake word sensitivity

--- animProcess/src/cozmoAnim/speechRecognizer/speechRecognizerPicovoice.cpp
+++ animProcess/src/cozmoAnim/speechRecognizer/speechRecognizerPicovoice.cpp
@@ -23,7 +23,7 @@ const char* model_path   = "/anki/data/assets/cozmo_resources/assets/picovoice/p
 const char* custom_ppn   = "/data/data/com.anki.victor/persistent/picovoice/custom_1-5-0.ppn";
 const char* default_ppn  = "/anki/data/assets/cozmo_resources/assets/picovoice/hey_vector.ppn";
 const char* sensitivity_path = "/data/data/com.anki.victor/persistent/picovoice/sensitivity";
-float default_sensitivity = 0.77f;
+float default_sensitivity = 0.45f;
 
 namespace Anki {
 namespace Vector {
