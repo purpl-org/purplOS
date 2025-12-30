58aa3e349cc65a241483c2ef908113e4f0f20a93
froggitti <85637569+froggitti@users.noreply.github.com>
2025-12-30 16:44:56 -0500

    try again

--- animProcess/src/cozmoAnim/backpackLights/animBackpackLightComponent.cpp
+++ animProcess/src/cozmoAnim/backpackLights/animBackpackLightComponent.cpp
@@ -484,7 +484,7 @@ void BackpackLightComponent::UpdateSystemLightState(bool isCloudStreamOpen)
       {
         // Pulsing cyan
         // purple
-        light.onColor = 0x80008000;
+        light.onColor = 80008000;
         light.offColor = 0x00FFFF00;
         light.onPeriod_ms = 960;
         light.offPeriod_ms = 960;
